20134e95612dfb9a7fa66dbeeea66321df7ef462
Пользователь <@notebook>
2018-10-13 21:28:24 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
-
+	unsigned int n;
+	uint64_t r;
+	cin >> n >> r;
+	uint64_t total_mass = 0;
+	while (n){
+		uint64_t w,h,d;
+		cin >> w >> h >> d;
+		total_mass += w * h * d * r;
+		--n;
+	}
+	cout << total_mass << endl;
 	return 0;
 }
