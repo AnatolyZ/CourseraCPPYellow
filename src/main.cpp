67677a737e0921a47a8d4169209030b179b59347
Anatoly <AMZharkov@gmail.com>
2018-10-14 18:20:49 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
-
 #include <iostream>
+#include <map>
 using namespace std;
+template <typename key, typename value>
+value& GetRefStrict(map<key,value>& m,const key& k){
+	if (m.count(k) == 0) throw runtime_error("Invalid key");
+	return m.at(k);
+}
 
 int main() {
-
+	map<int, string> m = {{0, "value"}};
+	string& item = GetRefStrict(m, 0);
+	item = "newvalue";
+	cout << m[0] << endl; // выведет newvalue
 	return 0;
 }
