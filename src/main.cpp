2e7c60607ccedf8ad4bd6eb7d5d6d561c4f8f10d
Anatoly <AMZharkov@gmail.com>
2018-10-16 22:59:51 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 
 #include <iostream>
+#include <algorithm>
+#include "sum_reverse_sort.h"
 using namespace std;
 
-int main() {
-
-	return 0;
+int Sum(int x, int y){
+	return x+y;
 }
+string Reverse(string s){
+	unsigned int size = s.size();
+	string result(s);
+	for(unsigned int i = 0; i < size; ++i){
+		result[size-1-i] = s[i];
+	}
+	return result;
+}
+void Sort(vector<int>& nums)
+{
+	sort(nums.begin(),nums.end());
+}
+
