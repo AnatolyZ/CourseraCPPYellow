a88e3b5d5050f792e4c1943c311b74ac29bc10f5
Anatoly <AMZharkov@gmail.com>
2018-10-17 22:52:56 +0300

    ver1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-
+#include <vector>
+#include <string>
 #include <iostream>
 using namespace std;
+
+vector<string> SplitIntoWords(const string& s);
+
+vector<string> SplitIntoWords(const string& s){
+	vector<string> result;
+	auto tmp_it = begin(s);
+	while (tmp_it != end(s)){
+		auto it = s.find(' ');
+		result.push_back({tmp_it,it});
+
+	}
+	return result;
+}
 
 int main() {
 
