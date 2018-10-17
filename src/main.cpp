eea776d158c15bc57ae75fdd260fa6945e44a3ab
Anatoly <AMZharkov@gmail.com>
2018-10-17 23:39:53 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 vector<string> SplitIntoWords(const string& s);
@@ -8,15 +9,29 @@ vector<string> SplitIntoWords(const string& s);
 vector<string> SplitIntoWords(const string& s){
 	vector<string> result;
 	auto tmp_it = begin(s);
-	while (tmp_it != end(s)){
-		auto it = s.find(' ');
-		result.push_back({tmp_it,it});
 
+	while (tmp_it != end(s)){
+		auto it = find(tmp_it, end(s),' ');
+		result.push_back({tmp_it,it});
+		if (it != end(s)){
+			tmp_it = ++it;
+		} else break;
 	}
 	return result;
 }
 
 int main() {
+  string s = "C Cpp Java Python";
 
-	return 0;
+  vector<string> words = SplitIntoWords(s);
+  cout << words.size() << " ";
+  for (auto it = begin(words); it != end(words); ++it) {
+    if (it != begin(words)) {
+      cout << "/";
+    }
+    cout << *it;
+  }
+  cout << endl;
+
+  return 0;
 }
