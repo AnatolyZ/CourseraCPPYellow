6498a05083b786217b1d6ad2784d58ace7b7c2b6
Anatoly <AMZharkov@gmail.com>
2018-10-18 19:21:33 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,48 @@
-
+#include <set>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
-int main() {
+set<int>::const_iterator FindNearestElement(const set<int>& numbers,
+		int border) {
+	 const auto first_not_less = numbers.lower_bound(border);
 
+	  // ≈сли множество пусто или до первого элемента не меньше border
+	  // нет элементов, то мы уже получили ответ
+	  if (first_not_less == numbers.begin()) {
+	    return first_not_less;
+	  }
+
+	  // ≈сли элементов, не меньших border, нет и set не пуст, то достаточно вз€ть
+	  // итератор на последний элемент, меньший border
+	  // prev -> http://ru.cppreference.com/w/cpp/iterator/prev
+	  const auto last_less = prev(first_not_less);
+	  if (first_not_less == numbers.end()) {
+	    return last_less;
+	  }
+
+	  // –азыменуем оба итератора-кандидата и выберем тот,
+	  // чей элемент ближе к искомому
+	  const bool is_left = (border - *last_less <= *first_not_less - border);
+	  return is_left ? last_less : first_not_less;
+
+
+}
+
+// set<int>::const_iterator Ч
+// тип итераторов дл€ константного множества целых чисел
+
+int main() {
+	set<int> numbers = { 1,7};
+	cout    << *FindNearestElement(numbers, 0) << " "
+			<< *FindNearestElement(numbers, 3) << " "
+			<< *FindNearestElement(numbers, 1) << " "
+			<< *FindNearestElement(numbers, 6) << " "
+			<< *FindNearestElement(numbers, 7) << endl;
+
+	set<int> empty_set;
+
+	cout << (FindNearestElement(empty_set, 8) == end(empty_set)) << endl;
 	return 0;
 }
+
