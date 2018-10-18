cbb1b03907f836d7076e69dc8ae22bc60f706f8a
Anatoly <AMZharkov@gmail.com>
2018-10-18 14:20:45 +0300

    ver1

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,14 @@ template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end){
 	if ((range_end - range_begin) >= 2) {
 		vector<typename RandomIt::value_type> elements(range_begin, range_end);
-		auto half_it = begin(elements) + (elements.size()/2);
-		MergeSort(begin(elements), half_it);
-		MergeSort(half_it, end(elements));
-		merge(begin(elements), half_it, half_it, end(elements),range_begin);
+		auto third1_it = begin(elements) + (elements.size()/3);
+		auto third2_it = third1_it + (elements.size()/3);
+		MergeSort(begin(elements), third1_it);
+		MergeSort(third1_it, third2_it);
+		MergeSort(third2_it, end(elements));
+		vector<typename RandomIt::value_type> tmp;
+		merge(begin(elements), third1_it, third1_it, third2_it,back_inserter(tmp));
+		merge(begin(tmp), end(tmp), third2_it, end(elements), range_begin);
 	}
 }
 
