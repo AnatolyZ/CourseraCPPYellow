3cd4ccec9ab5034a881e7a052da3ff5a1db5b3df
Anatoly <AMZharkov@gmail.com>
2018-10-18 13:10:59 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
-
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
+void FillVector(vector<int>& v) {
+	for (size_t i = 0; i < v.size(); ++i) {
+		v[i] = v.size() - i;
+	}
+}
 
+void PrintVector(const vector<int>& v) {
+	for (const auto& i : v) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+int main() {
+	size_t n;
+	cin >> n;
+	vector<int> vec(n);
+	FillVector(vec);
+	PrintVector(vec);
+
+	while (prev_permutation(begin(vec), end(vec))) {
+		PrintVector(vec);
+	}
 	return 0;
 }
