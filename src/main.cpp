14d51560d24bfa714c093d00d98edc6607be6789
Anatoly <AMZharkov@gmail.com>
2018-10-18 12:39:13 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
-
+#include <vector>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
-int main() {
+template <typename T>
+void RemoveDuplicates(vector<T>& elements);
 
-	return 0;
+template <typename T>
+void RemoveDuplicates(vector<T>& elements){
+	sort(begin(elements),end(elements));
+	auto it = unique(begin(elements),end(elements));
+	elements.erase(it,end(elements));
+}
+
+
+int main() {
+  vector<int> v1 = {6, 4, 7, 6, 4, 4, 0, 1};
+  RemoveDuplicates(v1);
+  for (int x : v1) {
+    cout << x << " ";
+  }
+  cout << endl;
+
+  vector<string> v2 = {"C", "C++", "C++", "C", "C++"};
+  RemoveDuplicates(v2);
+  for (const string& s : v2) {
+    cout << s << " ";
+  }
+  cout << endl;
+  return 0;
 }
