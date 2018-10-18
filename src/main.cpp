9598e33aa545f4c918dda49af831cef8cd5cddd4
Anatoly <AMZharkov@gmail.com>
2018-10-18 13:33:20 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,88 @@
 #include <algorithm>
 using namespace std;
 
-void FillVector(vector<int>& v) {
-	for (size_t i = 0; i < v.size(); ++i) {
-		v[i] = v.size() - i;
-	}
-}
+/*
+enum class Gender {
+	FEMALE, MALE
+};
 
-void PrintVector(const vector<int>& v) {
-	for (const auto& i : v) {
-		cout << i << ' ';
+struct Person {
+	int age;  // возраст
+	Gender gender;  // пол
+	bool is_employed;  // имеет ли работу
+};
+*/
+
+void PrintStats(vector<Person> persons);
+
+template<typename InputIt>
+int ComputeMedianAge(InputIt range_begin, InputIt range_end);
+
+void PrintStats(vector<Person> persons) {
+	cout << "Median age = " << ComputeMedianAge(begin(persons), end(persons))
+			<< endl;
+	auto it = partition(begin(persons), end(persons), [](const Person& p) {
+		return p.gender == Gender::FEMALE;
+	});
+
+	cout << "Median age for females = " << ComputeMedianAge(begin(persons), it)
+			<< endl;
+	it = partition(begin(persons), end(persons), [](const Person& p) {
+		return p.gender == Gender::MALE;
+	});
+
+	cout << "Median age for males = " << ComputeMedianAge(begin(persons), it)
+			<< endl;
+
+	it = partition(begin(persons), end(persons), [](const Person& p) {
+		return p.gender == Gender::FEMALE && p.is_employed;
+	});
+	cout << "Median age for employed females = "
+			<< ComputeMedianAge(begin(persons), it) << endl;
+
+	it = partition(begin(persons), end(persons), [](const Person& p) {
+		return p.gender == Gender::FEMALE && !p.is_employed;
+	});
+	cout << "Median age for unemployed females = "
+			<< ComputeMedianAge(begin(persons), it) << endl;
+
+	it = partition(begin(persons), end(persons), [](const Person& p) {
+		return p.gender == Gender::MALE && p.is_employed;
+	});
+	cout << "Median age for employed males = "
+			<< ComputeMedianAge(begin(persons), it) << endl;
+
+	it = partition(begin(persons), end(persons), [](const Person& p) {
+		return p.gender == Gender::MALE && !p.is_employed;
+	});
+	cout << "Median age for unemployed males = "
+			<< ComputeMedianAge(begin(persons), it) << endl;
+
+}
+/*
+// Это пример функции, его не нужно отправлять вместе с функцией PrintStats
+template<typename InputIt>
+int ComputeMedianAge(InputIt range_begin, InputIt range_end) {
+	if (range_begin == range_end) {
+		return 0;
 	}
-	cout << endl;
+	vector<typename InputIt::value_type> range_copy(range_begin, range_end);
+	auto middle = begin(range_copy) + range_copy.size() / 2;
+	nth_element(begin(range_copy), middle, end(range_copy),
+			[](const Person& lhs, const Person& rhs) {
+				return lhs.age < rhs.age;
+			});
+	return middle->age;
 }
 
 int main() {
-	size_t n;
-	cin >> n;
-	vector<int> vec(n);
-	FillVector(vec);
-	PrintVector(vec);
-
-	while (prev_permutation(begin(vec), end(vec))) {
-		PrintVector(vec);
-	}
+	vector<Person> persons = { { 31, Gender::MALE, false }, { 40,
+			Gender::FEMALE, true }, { 24, Gender::MALE, true }, { 20,
+			Gender::FEMALE, true }, { 80, Gender::FEMALE, false }, { 78,
+			Gender::MALE, false }, { 10, Gender::FEMALE, false }, { 55,
+			Gender::MALE, true }, };
+	PrintStats(persons);
 	return 0;
 }
+
+*/
