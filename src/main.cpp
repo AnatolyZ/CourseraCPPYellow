3f407fa49989876d51bcb241e9c1f762693db9a0
Anatoly <AMZharkov@gmail.com>
2018-10-18 12:27:07 +0300

    OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-
+#include <vector>
+#include <set>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
+bool f(int n) {
+	return n > 0;
+}
 
+int main() {
+	set<int> s = { 1, 5, 6, 7, -4, -1, 6 };
+	vector<int> v;
+//--------------------
+
+	v.assign(begin(s), end(s));
+	auto it = remove_if(begin(v), end(v), f);
+	v.erase(it, end(v));
+
+//--------------------
+	for (const auto& i : v) {
+		cout << i << ' ';
+	}
 	return 0;
 }
