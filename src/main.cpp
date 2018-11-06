3cf099d077546f0b80666217b3745fead4f232f8
Anatoly Zharkov <AMZharkov@gmail.com>
2018-11-06 23:16:10 +0300

    DONE

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,34 @@ using namespace std;
 
 template<typename RandomIt>
 pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin,
-		RandomIt range_end, char prefix) {
-	string s = " ";
-	s[0] = prefix;
-	return equal_range(range_begin, range_end, s,[](const auto& str1,const auto& str2){
-		return str1[0] < str2[0];
-	});
+		RandomIt range_end, const string& prefix) {
+	string s(prefix);
+	return equal_range(range_begin, range_end, s,
+			[s](const auto& str1,const auto& str2) {
+				return str1.substr(0,s.size()) < str2.substr(0,s.size());
+			});
 }
 
 int main() {
-	const vector<string> sorted_strings = { "moscow", "murmansk", "vologda" };
+	const vector<string> sorted_strings =
+			{ "moscow", "motovilikha", "murmansk" };
 
-	const auto m_result = FindStartsWith(begin(sorted_strings),
-			end(sorted_strings), 'm');
-	for (auto it = m_result.first; it != m_result.second; ++it) {
+	const auto mo_result = FindStartsWith(begin(sorted_strings),
+			end(sorted_strings), "mo");
+	for (auto it = mo_result.first; it != mo_result.second; ++it) {
 		cout << *it << " ";
 	}
 	cout << endl;
 
-	const auto p_result = FindStartsWith(begin(sorted_strings),
-			end(sorted_strings), 'p');
-	cout << (p_result.first - begin(sorted_strings)) << " "
-			<< (p_result.second - begin(sorted_strings)) << endl;
+	const auto mt_result = FindStartsWith(begin(sorted_strings),
+			end(sorted_strings), "mt");
+	cout << (mt_result.first - begin(sorted_strings)) << " "
+			<< (mt_result.second - begin(sorted_strings)) << endl;
 
-	const auto z_result = FindStartsWith(begin(sorted_strings),
-			end(sorted_strings), 'z');
-	cout << (z_result.first - begin(sorted_strings)) << " "
-			<< (z_result.second - begin(sorted_strings)) << endl;
+	const auto na_result = FindStartsWith(begin(sorted_strings),
+			end(sorted_strings), "na");
+	cout << (na_result.first - begin(sorted_strings)) << " "
+			<< (na_result.second - begin(sorted_strings)) << endl;
 
 	return 0;
 }
