99b094ab6c5cb99fc481caba36a0a10ed4214887
Anatoly Zharkov <AMZharkov@gmail.com>
2018-11-07 00:31:41 +0300

    DONE

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,46 @@
 
 using namespace std;
 
-
 int main() {
 	int initial_value;
 	cin >> initial_value;
 	int n;
 	cin >> n;
-	queue<pair<char,int>> equation_order;
-	while (n){
+	queue<pair<char, int>> equation_order;
+	while (n) {
 		char operation;
 		cin >> operation;
 		int value;
 		cin >> value;
-		equation_order.push(make_pair(operation,value));
+		equation_order.push(make_pair(operation, value));
 		--n;
 	}
 	n = equation_order.size();
 	string result;
-	result.insert(0,n,'(');
 	result += to_string(initial_value);
+	char last_operation = '*';
+	int parentheses_amount = 0;
 	while (n) {
-		pair<char,int>tmp_pair = equation_order.front();
+		pair<char, int> tmp_pair = equation_order.front();
 		equation_order.pop();
-		result.push_back(')');
-		result.push_back(' ');
-		result.push_back(tmp_pair.first);
-		result.push_back(' ');
-		result += to_string(tmp_pair.second);
+		if ((last_operation == '-' || last_operation == '+')
+				&& (tmp_pair.first == '*' || tmp_pair.first == '/')) {
+			++parentheses_amount;
+			result.push_back(')');
+			result.push_back(' ');
+			result.push_back(tmp_pair.first);
+			result.push_back(' ');
+			result += to_string(tmp_pair.second);
+		} else {
+			result.push_back(' ');
+			result.push_back(tmp_pair.first);
+			result.push_back(' ');
+			result += to_string(tmp_pair.second);
+		}
+		last_operation = tmp_pair.first;
 		--n;
 	}
+	result.insert(0, parentheses_amount, '(');
 	cout << result;
 	return 0;
 }
